30e4d772f7d22e81b0128043b35fbfd00b194c36
juanvegu <juanvegu.dev@gmail.com>
2023-10-05 12:41:47 -0300

    permission
    all methods implemented

--- lib/app/permission/permission_app.dart
+++ lib/app/permission/permission_app.dart
@@ -2,5 +2,8 @@ import 'package:motow_app/app/permission/permission_state.dart';
 import 'package:motow_app/app/permission/permission_type.dart';
 
 abstract class PermissionApp {
-  Future<PermissionState> getPermissionState(PermissionType permissionType);
+  Future<PermissionState> permissionState(PermissionType type);
+  Future<PermissionState> requestPermission(PermissionType type);
+  Future<bool> serviceStatus(PermissionType type);
+  Future<bool> openAppSettings();
 }
--- lib/app/permission/permission_handler.dart
+++ lib/app/permission/permission_handler.dart
@@ -6,11 +6,43 @@ import 'package:permission_handler/permission_handler.dart';
 
 class PermissionHandler implements PermissionApp {
   @override
-  Future<PermissionState> getPermissionState(PermissionType permissionType) async {
-    switch (permissionType) {
+  Future<PermissionState> permissionState(PermissionType type) async {
+    late PermissionStatus status;
+    switch (type) {
       case PermissionType.location:
-        final status = await Permission.location.status;
-        return status.toPermissionState();
+        status = await Permission.location.status;
+        break;
     }
+
+    return status.toPermissionState();
+  }
+
+  @override
+  Future<PermissionState> requestPermission(PermissionType type) async {
+    late PermissionStatus status;
+    switch (type) {
+      case PermissionType.location:
+        status = await Permission.location.request();
+        break;
+    }
+
+    return status.toPermissionState();
+  }
+
+  @override
+  Future<bool> serviceStatus(PermissionType type) async {
+    late bool serviceStatus;
+    switch (type) {
+      case PermissionType.location:
+        serviceStatus = await Permission.location.serviceStatus.isEnabled;
+        break;
+    }
+
+    return serviceStatus;
+  }
+
+  @override
+  Future<bool> openAppSettings() async {
+    return await openAppSettings();
   }
 }
